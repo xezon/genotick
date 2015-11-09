6246102dbf23a076f1fd5c2af09caceb491e1e49
Lukasz <lukasz.wojtow@gmail.com>
2015-11-09 20:46:30 +0000

    Added new method to DataSetExecutor. This method accepts and returns List of things that are interesting.

--- src/main/java/com/alphatica/genotick/genotick/DataSetExecutor.java
+++ src/main/java/com/alphatica/genotick/genotick/DataSetExecutor.java
@@ -2,10 +2,13 @@ package com.alphatica.genotick.genotick;
 
 import com.alphatica.genotick.population.*;
 
+import java.util.List;
+
 public interface DataSetExecutor {
 
     ProgramResult execute(ProgramData programData, ProgramName programName, Population population);
 
+    List<ProgramResult> execute(List<ProgramData> programDataList, Program program, ProgramExecutor programExecutor);
     void setExecutorFactory(ProgramExecutorSettings settings);
 
 }
--- src/main/java/com/alphatica/genotick/genotick/SimpleDataSetExecutor.java
+++ src/main/java/com/alphatica/genotick/genotick/SimpleDataSetExecutor.java
@@ -3,6 +3,9 @@ package com.alphatica.genotick.genotick;
 import com.alphatica.genotick.population.*;
 import com.alphatica.genotick.processor.ProgramExecutorFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class  SimpleDataSetExecutor implements DataSetExecutor {
 
     private ProgramExecutorSettings settings;
@@ -14,6 +17,18 @@ public class  SimpleDataSetExecutor implements DataSetExecutor {
         ProgramResult result = execute(programData,program);
         population.saveProgram(program);
         return result;
+    }
+
+    @Override
+    public List<ProgramResult> execute(List<ProgramData> programDataList, Program program, ProgramExecutor programExecutor) {
+        List<ProgramResult> programResultList = new ArrayList<>(programDataList.size());
+        for(ProgramData programData: programDataList) {
+            Prediction prediction = programExecutor.executeProgram(programData,program);
+            program.recordPrediction(prediction);
+            ProgramResult result = new ProgramResult(prediction,program,programData);
+            programResultList.add(result);
+        }
+        return programResultList;
     }
 
     @Override
