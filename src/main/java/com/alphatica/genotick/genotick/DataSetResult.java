c5ea2893f9c7536e7efc9fd62fb32ca57e9a65c8
Lukasz Wojtow <lukasz.wojtow@gmail.com>
2016-02-09 23:02:21 +0000

    Changed how cumulative prediction is calculated.

--- src/main/java/com/alphatica/genotick/genotick/DataSetResult.java
+++ src/main/java/com/alphatica/genotick/genotick/DataSetResult.java
@@ -6,36 +6,59 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataSetResult {
-    private final List<ProgramResult> list;
     private final DataSetName name;
+    private double weightUp;
+    private double weightOut;
+    private int countOut;
+    private double weightDown;
+    private int countDown;
+    private int countUp;
 
     public DataSetResult(DataSetName name) {
-        list = new ArrayList<>();
         this.name = name;
     }
 
     public void addResult(ProgramResult programResult) {
-        list.add(programResult);
+        Double weight = programResult.getWeight();
+        if(weight.isNaN())
+            return;
+        processWeight(programResult);
+    }
+
+    private void processWeight(ProgramResult programResult) {
+        if(programResult.getWeight() > 0) {
+            switch (programResult.getPrediction()) {
+                case UP: recordUp(programResult.getWeight()); break;
+                case DOWN: recordDown(programResult.getWeight()); break;
+                case OUT: recordOut(programResult.getWeight());
+            }
+        }
+        if(programResult.getWeight() < 0) {
+            switch (programResult.getPrediction()) {
+                case UP: recordDown(-programResult.getWeight()); break;
+                case DOWN: recordUp(-programResult.getWeight()); break;
+                case OUT: recordOut(programResult.getWeight());
+            }
+        }
+    }
+
+    private void recordOut(double weight) {
+        weightOut += weight;
+        countOut++;
+    }
+
+    private void recordDown(double weight) {
+        weightDown += weight;
+        countDown++;
+    }
+
+    private void recordUp(double weight) {
+        weightUp += weight;
+        countUp++;
     }
 
     public Prediction getCumulativePrediction() {
-        double weightUp = 0;
-        double weightDown = 0;
-        double weightOut = 0;
-        double direction = 0;
-        int countUp = 0;
-        int countDown = 0;
-        int countOut = 0;
-        for(ProgramResult result: list) {
-            if(result.getWeight().isNaN())
-                continue;
-            switch(result.getPrediction()) {
-                case UP: weightUp+= result.getWeight(); countUp++; break;
-                case DOWN: weightDown += result.getWeight(); countDown++; break;
-                default: weightOut += result.getWeight(); countOut++; break;
-            }
-            direction = weightUp - weightDown;
-        }
+        double direction = weightUp - weightDown;
         Debug.d("Up:",weightUp,countUp,"Down:",weightDown,countDown,"Out:",weightOut,countOut);
         return Prediction.getPrediction(direction);
     }
