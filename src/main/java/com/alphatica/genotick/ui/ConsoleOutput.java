d5c573c3326eb1ee90c37a748e5393713883829e
Łukasz Wojtów <lukasz.wojtow@gmail.com>
2016-11-26 23:36:37 +0100

    Add logging to a file

--- src/main/java/com/alphatica/genotick/ui/ConsoleOutput.java
+++ src/main/java/com/alphatica/genotick/ui/ConsoleOutput.java
@@ -2,44 +2,63 @@ package com.alphatica.genotick.ui;
 
 import com.alphatica.genotick.data.DataSetName;
 import com.alphatica.genotick.genotick.Prediction;
+import com.alphatica.genotick.genotick.Tools;
 import com.alphatica.genotick.timepoint.TimePoint;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 
 class ConsoleOutput implements UserOutput {
 
+    private File logFile = new File(String.format("genotick-log-%s.txt", Tools.getPidString()));
+
     @Override
     public void errorMessage(String message) {
-        System.out.println("Error: " + message);
+        log("Error: " + message);
     }
 
     @Override
     public void warningMessage(String message) {
-        System.out.println("Warning: " + message);
+        log("Warning: " + message);
     }
 
     @Override
     public void reportProfitForTimePoint(TimePoint timePoint, double cumulativeProfit, double timePointProfit) {
-        System.out.println("Profit for " + timePoint.toString() + ": "
+        log("Profit for " + timePoint.toString() + ": "
                 + "Cumulative profit: " + cumulativeProfit + " "
                 + "TimePoint's profit: " + timePointProfit);
     }
 
     @Override
     public void showPrediction(TimePoint timePoint, DataSetName name, Prediction prediction) {
-        System.out.println(String.format("%s prediction on %s for the next trade: %s",
+        log(String.format("%s prediction on %s for the next trade: %s",
                 name.toString(),timePoint.toString(),prediction.toString()));
     }
 
     @Override
     public Thread.UncaughtExceptionHandler createExceptionHandler() {
         return (thread, throwable) -> {
-            System.out.println("Exception in thread: " + thread.getName());
-            throwable.printStackTrace();
+            log("Exception in thread: " + thread.getName());
+            for(StackTraceElement element: throwable.getStackTrace()) {
+                log(element.toString());
+            }
         };
     }
 
     @Override
     public void infoMessage(String s) {
         System.out.println(s);
+    }
+
+    private void log(String string) {
+        System.out.println(string);
+        try {
+            FileUtils.write(logFile, string + System.lineSeparator(), true);
+        } catch (IOException e) {
+            System.err.println("Unable to write to file " + logFile.getPath() + ": " + e.getMessage());
+            System.exit(1);
+        }
     }
 
 }
