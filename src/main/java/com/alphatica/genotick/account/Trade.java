eef59c148060ca18dc2b168b7cb4b9740afd33d5
xezon <pascalkross@gmail.com>
2017-09-08 12:51:49 +0200

    Revert change in Trade.java

--- src/main/java/com/alphatica/genotick/account/Trade.java
+++ src/main/java/com/alphatica/genotick/account/Trade.java
@@ -1,30 +1,14 @@
 package com.alphatica.genotick.account;
 
 import lombok.Value;
-
 import java.math.BigDecimal;
 
 @Value
 class Trade {
-
     private BigDecimal quantity;
-
     private BigDecimal price;
 
     BigDecimal value() {
         return quantity.abs().multiply(price);
-    }
-    
-    public Trade(BigDecimal quantity, BigDecimal price) {
-        this.quantity = quantity;
-        this.price = price;
-    }
-    
-    public BigDecimal getPrice() {
-        return price;
-    }
-    
-    public BigDecimal getQuantity() {
-        return quantity;
     }
 }
