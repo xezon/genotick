f71d9b8fecadcbc5cacf3c187738a393b578b833
xezon <pascalkross@gmail.com>
2017-09-12 12:52:26 +0200

    Add missing import

--- src/main/java/com/alphatica/genotick/account/ProfitRecorder.java
+++ src/main/java/com/alphatica/genotick/account/ProfitRecorder.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import static java.lang.String.format;
 import static java.util.Optional.ofNullable;
 
 public class ProfitRecorder {
