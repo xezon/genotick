b0bb4f676e947e0be2cca4b17236983163e1cb4c
xezon <pascalkross@gmail.com>
2017-10-28 14:22:15 +0200

    Remove static member from DaemonThreadFactory

--- src/main/java/com/alphatica/genotick/timepoint/DaemonThreadFactory.java
+++ src/main/java/com/alphatica/genotick/timepoint/DaemonThreadFactory.java
@@ -3,12 +3,11 @@ package com.alphatica.genotick.timepoint;
 import java.util.concurrent.ThreadFactory;
 
 class DaemonThreadFactory implements ThreadFactory {
-    private static int counter = 1;
     @Override
     public Thread newThread(@SuppressWarnings("NullableProblems") Runnable runnable) {
         Thread thread = new Thread(runnable);
         thread.setDaemon(true);
-        thread.setName("TimePointExecutor thread: " + String.valueOf(counter++));
+        thread.setName("TimePointExecutor thread " + thread.getId());
         return thread;
     }
 }
