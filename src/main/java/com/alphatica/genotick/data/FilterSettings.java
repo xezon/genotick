6233f86a6d3c9391f94963f6c73e834720d1e6c6
xezon <pascalkross@gmail.com>
2017-12-21 18:21:25 +0100

    First pass on filtered data review (#65)
    * First pass on filtered data review
    
    * Second pass on review

--- src/main/java/com/alphatica/genotick/data/FilterSettings.java
+++ src/main/java/com/alphatica/genotick/data/FilterSettings.java
@@ -8,6 +8,10 @@ public class FilterSettings {
     // TODO add bit mask for filter columns
     // TODO add settings for filter specific variables
     
+    public FilterSettings() {
+        filterOption = FilterOption.NONE;
+    }
+    
     public FilterSettings(final MainSettings settings) {
         filterOption = settings.filterOption;
     }
